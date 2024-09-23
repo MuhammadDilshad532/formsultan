622c584334d921e017640148ad5211b712c7e1b7
Dilshad <dilshadahamad532280@gmail.com>
2024-09-23 18:00:58 +0500

    ramove z index

--- src/Form.jsx
+++ src/Form.jsx
@@ -109,7 +109,7 @@ const Form = () => {
             onChange={handleChange}
             className="block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:border-blue-600"
           />
-          <label className="absolute text-sm text-white duration-300 transform -translate-y-6 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-6">
+          <label className="absolute text-sm text-white duration-300 transform -translate-y-6 top-3  origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-6">
             Your Email
           </label>
           <AiOutlineMail className="absolute top-4 right-4" />
