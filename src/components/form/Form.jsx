35e0a16e4b3cc87a1a40599789ee23cd1455b41c
Dilshad <dilshadahamad532280@gmail.com>
2024-09-30 19:05:49 +0500

    use map for input

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -63,7 +63,13 @@ const Form = () => {
       });
     }
   };
-
+const Fields = [
+  {label:"Your First Name" , type:"text" , name:"firstname"},
+  {label:"Your Last Name" , type:"text" , name:"lastname"},
+  {label:"Your Email" , type:"email" , name:"email", icon:<AiOutlineMail /> , error:errors.email },
+  {label:"Your Password" , type:"password" , name:"password" , icon:<AiOutlineLock />},
+  {label:"Confirm Your password" , type:"password" , name:"confirmPassword" , error:errors.password},
+]
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -73,55 +79,25 @@ const Form = () => {
       <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
         <h1 className="text-4xl text-white font-bold text-center mb-6">Form</h1>
         <form onSubmit={handleSubmit}>
-          <InputField
-            label="Your First Name"
-            type="text"
-            name="firstName"
-            value={formData.firstName}
+         {
+          Fields.map((field , index) =>{
+            <InputField 
+            key={index}
+            label={field.label}
+            type={field.type}
+            name={field.name}
+            value={formData[field.name]}
             onChange={handleChange}
-          />
-          <InputField
-            label="Your Last Name"
-            type="text"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleChange}
-          />
-          <InputField
-            label="Your Email"
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            error={errors.email}
-            icon={<AiOutlineMail />}
-          />
-          <InputField
-            label="Your Password"
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-          <InputField
-            label="Confirm Your Password"
-            type="password"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            error={errors.password}
-            icon={<AiOutlineLock />}
-          />
-
-          {/* Submit Button */}
+            error={field.error}
+            icon={field.icon}
+            />
+          })}
           <button
             type="submit"
             className="w-full mb-4 text-[18px] mt-6 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white"
           >
             Save
           </button>
-
-          {/* Success Message */}
           {successMessage && (
             <span className="text-green-600 text-center block mt-4">
               {successMessage}
