531c09a861681379c0a13fe9cb5c6249e6763102
Dilshad <dilshadahamad532280@gmail.com>
2024-10-03 13:31:43 +0500

    using usestate pass data

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineLock, AiOutlineMail } from "react-icons/ai";
 import InputField from "../InputField/InputField";
 import { useNavigate } from "react-router-dom";
 
-let formData = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-};
-
-let errors = {
-  email: "",
-  password: "",
-};
-
-let successMessage = "";
-let submittedData = [];
-
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 const Form = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [successMessage, setSuccessMessage] = useState("");
+  const [submittedData, setSubmittedData] = useState([]);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const navigate = useNavigate();
 
   const validateForm = () => {
@@ -47,7 +45,7 @@ const Form = () => {
       isValid = false;
     }
 
-    errors = newErrors;
+    setErrors(newErrors);
     return isValid;
   };
 
@@ -55,20 +53,20 @@ const Form = () => {
     e.preventDefault();
 
     if (validateForm()) {
-      successMessage = "Your data is saved!";
-      submittedData.push(formData);
-      formData = {
+      setSuccessMessage("Your data is saved!");
+      setSubmittedData([...submittedData, formData]);
+      setFormData({
         firstName: "",
         lastName: "",
         email: "",
         password: "",
         confirmPassword: "",
-      };
-      alert(successMessage); // For feedback since there's no re-render
+      });
     }
   };
 
   const handleNext = () => {
+    
     navigate("/submitted-data", { state: { submittedData } });
   };
 
@@ -81,40 +79,47 @@ const Form = () => {
   ];
 
   const handleChange = (e) => {
-    formData[e.target.name] = e.target.value;
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   return (
-    <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
-      <h1 className="text-4xl text-white font-bold text-center mb-6">Form</h1>
-      <form onSubmit={handleSubmit}>
-        {fields.map((field, index) => (
-          <InputField
-            key={index}
-            label={field.label}
-            type={field.type}
-            name={field.name}
-            value={formData[field.name]}
-            onChange={handleChange}
-            error={field.error}
-            icon={field.icon}
-          />
-        ))}
-        <button
-          type="submit"
-          className="w-full mb-4 text-[18px] mt-6 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white"
-        >
-          Save
-        </button>
-        <button
-          type="button"
-          onClick={handleNext}
-          className="w-full mb-4 text-[18px] mt-2 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white"
-        >
-          Next
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
+        <h1 className="text-4xl text-white font-bold text-center mb-6">Form</h1>
+        <form onSubmit={handleSubmit}>
+          {fields.map((field, index) => (
+            <InputField
+              key={index}
+              label={field.label}
+              type={field.type}
+              name={field.name}
+              value={formData[field.name]}
+              onChange={handleChange}
+              error={field.error}
+              icon={field.icon}
+            />
+          ))}
+          <button
+            type="submit"
+            className="w-full mb-4 text-[18px] mt-6 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white"
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            onClick={handleNext}
+            className="w-full mb-4 text-[18px] mt-2 rounded-full py-2 transition-colors duration-300 bg-white text-emerald-800 hover:bg-emerald-600 hover:text-white"
+          >
+            Next
+          </button>
+          {successMessage && (
+            <span className="text-green-600 text-center block mt-4">
+              {successMessage}
+            </span>
+          )}
+        </form>
+      </div>
+    </>
   );
 };
 
